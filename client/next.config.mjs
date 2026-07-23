b017b2d3883910ac5c19bb1f8cec9456e135f165
ClaudioBe <bernalclaudio044@gmail.com>
2026-07-23 19:29:43 -0300

    change config next

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,15 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+};
 
 export default nextConfig;
