18c954971f7ccdf74898ec177465fda34e43ffda
ClaudioBe <bernalclaudio044@gmail.com>
2026-07-23 21:06:41 -0300

    config next

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,13 @@
-/** @type {import('next').NextConfig} */
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
   reactStrictMode: true,
   turbopack: {
     root: __dirname,
-  }
+  },
 };
 
 export default nextConfig;
